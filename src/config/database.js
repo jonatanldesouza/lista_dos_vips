69ad99ebb4d7122e87ee2fb1d43e181a95d684b9
jonatanldesouza <jonatanl.souza@gmail.com>
2018-10-31 19:40:17 -0300

    heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://localhost/db_listadosvips')
+mongoose.Promise = global.Promise
+
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI:'mongodb://localhost/db_listadosvips'
+
+module.exports = mongoose.connect(url, {useMongoClient:true})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'"
